0024ad8f425486bf2262585512575b1b58a48711
Talamantez <robert.talamantez2@gmail.com>
2014-11-03 17:39:26 -0800

    test blog

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+'use strict'
+
 var express = require('express');
 var app = express();
+var port = 3000;
 
-app.get('/', function(req,res){
-	res.send('Portfolio site under construction.');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(process.env.PORT || '8000');
+app.listen(port, function(){
+	console.log('Listening on port %d', port);
+});
