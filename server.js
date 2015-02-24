b3dd937b22a3712393ceabb181a7e5841c7d0d62
root <root@ip-172-31-33-201.us-west-2.compute.internal>
2015-02-24 03:14:48 +0000

    ec2 52.10.113.199

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 
 var express = require('express'),
     exphbs  = require('express-handlebars');
- 
+var http = require('http');
 var app = express();
-
+app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
  
 app.get('/', function (req, res) {
-    res.render('home');
+	 res.render('home');
 });
-    
-app.listen(3000);
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+	console.log('Server running on ' + app.get('port'));
+});
